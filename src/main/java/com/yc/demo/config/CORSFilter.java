3885f04a1862886b27de4c1a1619d45058d4ffcb
chen.yan <410265915@qq.com>
2021-05-08 20:30:48 +0800

    22222

---
+++ src/main/java/com/yc/demo/config/CORSFilter.java
@@ -0,0 +1,39 @@
+package com.yc.demo.config;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Yanchen
+ * date 2021/5/8 20:26
+ */
+@Component
+public class CORSFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) response;
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        res.setHeader("Access-Control-Max-Age", "3600");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type, x-requested-with, X-Custom-Header,at,tid");
+        //添加请求头 给前端获取文件流名称
+        res.setHeader("Access-Control-Expose-Headers", "Content-Disposition");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+}
+
